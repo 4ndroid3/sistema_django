e4c89edfd3d3dc0f73ba642069d0b613013593ca
4ndroid3 <andijaimo@gmail.com>
2021-03-23 19:09:57 -0300

    se agregan campos en la clase profile

--- Users/models.py
+++ Users/models.py
@@ -23,7 +23,38 @@ class Profile(models.Model):
     
     id_users = models.OneToOneField(
         'User',
+        default = None,
         on_delete = models.CASCADE,
         help_text = 'User name / Account name',
         verbose_name = 'User',
     )
+
+    birth_date = models.DateField(
+        blank = True,
+        null=True,
+        verbose_name = 'Birth date',
+        help_text = 'Date of birth',
+    )
+
+    biography = models.TextField(
+        default = '',
+        max_length = 500,
+        blank=True,
+        help_text = 'Short resume or history about the User',
+        verbose_name = 'Biography',
+    )
+
+    profile_img = models.ImageField(
+        upload_to = 'profile_img', 
+        blank = True,
+        null=True,
+        help_text = 'A image of the user',
+        verbose_name = 'Profile Image',
+    )
+
+    def __str__(self):
+        return str(self.id_users)
+    
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'

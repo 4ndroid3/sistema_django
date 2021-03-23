108fb9a0a2e8d0574c3c6da23dd63ee2ab2efe01
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2021-03-23 01:46:05 -0300

    added Role and User models

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,9 @@
+from Users.models import Role, User
 from django.contrib import admin
 
 # Register your models here.
+from .models import Role, User
+
+
+admin.site.register(Role)
+admin.site.register(User)
--- Users/models.py
+++ Users/models.py
@@ -4,9 +4,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+# TODO: add login using email
+# TODO: add roles to User
+
+
+class Role(models.Model):
+    """ Types of Roles """
+    ADMIN = 1
+    GERENTE = 2
+    CAJERO = 3
+
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (GERENTE, 'Gerente'),
+        (CAJERO, 'Cajero'),
+    )
+    id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
+
+
 class User(AbstractUser):
     """ Custom User """
-    pass
+    role = models.ManyToManyField(Role)
+
 
 class Profile(models.Model):
     """ Profile Model """

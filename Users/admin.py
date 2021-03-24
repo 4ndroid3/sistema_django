acc0dace7569a11ce51a183993a5d67fc80fd094
4ndroid3 <andijaimo@gmail.com>
2021-03-24 08:18:38 -0300

    create Custom Admin for Profile Model

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,14 @@
+""" Users Admin"""
+
+# Django Imports
 from django.contrib import admin
 
-# Register your models here.
+# Project Imports
+from .models import Profile
+
+class CustomProfileAdmin(admin.ModelAdmin):
+    """ Custom display of elements in the admin page"""
+
+    list_display = ('id_users', 'birth_date',)
+
+admin.site.register(Profile, CustomProfileAdmin)
